213763699ccf9b4ba1ba40a58b11f0f0835dcb8a
JuanRoldan <jroldanmendoza0925@gmail.com>
2024-02-27 22:34:39 -0500

    log4j

--- src/dao/OdontologoDaoH2.java
+++ src/dao/OdontologoDaoH2.java
@@ -1,6 +1,7 @@
 package dao;
 
 import model.Odontologo;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao <Odontologo> {
+    private static final Logger logger=Logger.getLogger(OdontologoDaoH2.class);
 private String CREAR="INSERT INTO ODONTOLOGO ( APELLIDO,NOMBRE,MATRICULA ) VALUES (?,?,?);";
 private String LISTAR= "SELECT * FROM ODONTOLOGO";
     @Override
@@ -21,6 +23,9 @@ private String LISTAR= "SELECT * FROM ODONTOLOGO";
         stmt.setString(1,data.getApellido());
             stmt.setString(2,data.getNombre());
             stmt.setString(3,data.getMatricula());
+
+            logger.info("creando odontologo en H2");
+
             stmt.execute();
             System.out.println("guardando");
             ResultSet resultSet= stmt.getGeneratedKeys();
@@ -28,12 +33,13 @@ private String LISTAR= "SELECT * FROM ODONTOLOGO";
                 data.setId (resultSet.getInt(1));
             }
         } catch (Exception e) {
+            logger.error("OCURRIO EL SIGUIENTE ERROR: ",e);
            e.printStackTrace();
         } finally {
             try {
                 con.close();
             } catch (Exception e) {
-
+                logger.error("OCURRIO EL SIGUIENTE ERROR: ",e);
                 e.printStackTrace();
             }
         }
@@ -47,6 +53,7 @@ private String LISTAR= "SELECT * FROM ODONTOLOGO";
         try {
             con=BD.conectar();
             PreparedStatement stmt=con.prepareStatement(this.LISTAR);
+            logger.info("consultando Odontologos en H2");
             ResultSet resultSet = stmt.executeQuery();
             while (resultSet.next()) {
                 odontologos.add(new Odontologo(resultSet.getString(2),resultSet.getString(3),resultSet.getString(4)));
@@ -54,11 +61,12 @@ private String LISTAR= "SELECT * FROM ODONTOLOGO";
 
         } catch (Exception e) {
             e.printStackTrace();
+            logger.error("OCURRIO EL SIGUIENTE ERROR: ",e);
         } finally {
             try {
                 con.close();
             } catch (Exception e) {
-
+                logger.error("OCURRIO EL SIGUIENTE ERROR: ",e);
                 e.printStackTrace();
             }
         }
--- src/dao/OdontologoDaoList.java
+++ src/dao/OdontologoDaoList.java
@@ -1,23 +1,27 @@
 package dao;
 
 import model.Odontologo;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OdontologoDaoList implements IDao<Odontologo> {
-private List <Odontologo> odontologos;
+    private static final Logger logger=Logger.getLogger(OdontologoDaoList.class);
+    private List <Odontologo> odontologos;
 public OdontologoDaoList() {
     this.odontologos=new ArrayList<>();
 }
     @Override
     public Odontologo guardar(Odontologo data) {
-        this.odontologos.add(data);
+        logger.info("creando Odontologo en array list");
+    this.odontologos.add(data);
         return data;
     }
 
     @Override
     public List<Odontologo> listar() {
-        return this.odontologos;
+        logger.info("consultando Odontologos en array list");
+    return this.odontologos;
     }
 }
